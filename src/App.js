7337ecfc180e1da9c91724a382bc080bc127f77f
Ashley <lauraashleywalker4@gmail.com>
2022-07-13 16:09:48 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './Components/Nav';
 import About from './Components/About';
+import Contact from './Components/Contact';
+import Projects from './Components/Projects';
+import Resume from './Components/Resume';
 
 function App() {
-  return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
-}
+      const [pages] = useState([
+        'About Me',
+        'Projects',
+        'Resume',
+        'Contact'
+      ]);
+      }
+ 
+const [currentPage, setCurrentPage] = useState(pages[0]);
 
-export default App;
+return(
+    <html>
+      <Headers
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      />
+      <body>
+        {currentPage === 'About Me' && <About />}
+        {currentPage === 'Projects' && <Projects />}
+        {currentPage === 'Resume' && <Resume />}
+        {currentPage === 'Contact Me' && <Contact />}
+      </body>
+    </html>
+
+);
+
+    
+export default App
